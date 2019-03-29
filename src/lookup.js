07f86a617f5fdf234d4be3abc710d49cac039e05
Micaela Jawor <micaelajawor@yahoo.com>
2019-03-29 16:50:55 -0700

    add doctor details

--- src/lookup.js
+++ src/lookup.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
 
     let request = new XMLHttpRequest();
     const url = `http://api.betterdoctor.com/2016-03-01/doctors?location=${city}&appid=${process.env.API_KEY}`;
-
+    var doctor_uid = '333d4bb6fcf640e18e93b11b00fe09eb'
+var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors/'+ doctor_uid + '?user_key=' + api_key; 
     $.get(resource_url, function (data) {
     // data: { meta: {<metadata>}, data: {<array[Practice]>} }
     var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
